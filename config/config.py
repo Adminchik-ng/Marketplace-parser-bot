e983b441949b30fee3242305043e33d42a0bb0c2
Dimas <dimas@mail.com>
2025-09-21 18:13:47 +0300

    version to deploy

--- config/config.py
+++ config/config.py
@@ -44,9 +44,17 @@ class Config:
     redis: RedisSettings
     log: LoggSettings
 
+@dataclass
+class TestConfig:
+    db: DatabaseSettings
+    redis: RedisSettings
 
-def load_config(path: str | None = None) -> Config:
+
+def load_config(path: str | None = None) -> Config | TestConfig:
     env = Env()
+    
+    # Определяем тип окружения по имени файла
+    is_test = path and "test" in path
 
     if path:
         if not os.path.exists(path):
@@ -55,45 +63,69 @@ def load_config(path: str | None = None) -> Config:
             logger.info("Loading .env from '%s'", path)
 
     env.read_env(path)
-
-    token = env("BOT_TOKEN")
-
-    if not token:
-        raise ValueError("BOT_TOKEN must not be empty")
-
-    raw_ids = env.list("ADMIN_IDS", default=[])
-
-    try:
-        admin_ids = [int(x) for x in raw_ids]
-    except ValueError as e:
-        raise ValueError(f"ADMIN_IDS must be integers, got: {raw_ids}") from e
     
-    db = DatabaseSettings(
-        name=env("POSTGRES_DB"),
-        host=env("POSTGRES_HOST"),
-        port=env.int("POSTGRES_PORT"),
-        user=env("POSTGRES_USER"),
-        password=env("POSTGRES_PASSWORD"),
-    )
+    if is_test:
+        # Для тестового окружения используем только необходимые переменные
+        db = DatabaseSettings(
+            name=env("POSTGRES_DB"),
+            host=env("POSTGRES_HOST"),
+            port=env.int("POSTGRES_PORT"),
+            user=env("POSTGRES_USER"),
+            password=env("POSTGRES_PASSWORD"),
+        )
 
-    redis = RedisSettings(
-        host=env("REDIS_HOST"),
-        port=env.int("REDIS_PORT"),
-        db=env.int("REDIS_DATABASE"),
-        password=env("REDIS_PASSWORD", default=""),
-        username=env("REDIS_USERNAME", default=""),
-    )
+        redis = RedisSettings(
+            host=env("REDIS_HOST"),
+            port=env.int("REDIS_PORT"),
+            db=env.int("REDIS_DATABASE"),
+            password=env("REDIS_PASSWORD", default=""),
+            username=env("REDIS_USERNAME", default=""),
+        )
 
-    logg_settings = LoggSettings(
-        level=env("LOG_LEVEL"),
-        format=env("LOG_FORMAT")
-    )
-
-    logger.info("Configuration loaded successfully")
-
-    return Config(
-        bot=BotSettings(token=token, admin_ids=admin_ids),
+        return TestConfig(
         db=db,
         redis=redis,
-        log=logg_settings
     )
+    else:
+
+        token = env("BOT_TOKEN")
+
+        if not token:
+            raise ValueError("BOT_TOKEN must not be empty")
+
+        raw_ids = env.list("ADMIN_IDS", default=[])
+
+        try:
+            admin_ids = [int(x) for x in raw_ids]
+        except ValueError as e:
+            raise ValueError(f"ADMIN_IDS must be integers, got: {raw_ids}") from e
+        
+        db = DatabaseSettings(
+            name=env("POSTGRES_DB"),
+            host=env("POSTGRES_HOST"),
+            port=env.int("POSTGRES_PORT"),
+            user=env("POSTGRES_USER"),
+            password=env("POSTGRES_PASSWORD"),
+        )
+
+        redis = RedisSettings(
+            host=env("REDIS_HOST"),
+            port=env.int("REDIS_PORT"),
+            db=env.int("REDIS_DATABASE"),
+            password=env("REDIS_PASSWORD", default=""),
+            username=env("REDIS_USERNAME", default=""),
+        )
+
+        logg_settings = LoggSettings(
+            level=env("LOG_LEVEL"),
+            format=env("LOG_FORMAT")
+        )
+
+        logger.info("Configuration loaded successfully")
+
+        return Config(
+            bot=BotSettings(token=token, admin_ids=admin_ids),
+            db=db,
+            redis=redis,
+            log=logg_settings
+        )
